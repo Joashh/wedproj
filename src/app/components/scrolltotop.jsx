59b6a645a53c1cb2947d514dc90f17456dc7dbfd
Julius Austria <arnelwowie@gmail.com>
2025-09-14 14:19:19 +0800

    scroll

--- src/app/components/scrolltotop.jsx
+++ src/app/components/scrolltotop.jsx
@@ -4,8 +4,11 @@ import { useEffect } from 'react';
 export default function ScrollToTop({ children }) {
   useEffect(() => {
     if ('scrollRestoration' in window.history) {
+      // Disable browser auto-restoring scroll
       window.history.scrollRestoration = 'manual';
     }
+
+    // Always scroll to top on first render
     window.scrollTo(0, 0);
   }, []);
 
